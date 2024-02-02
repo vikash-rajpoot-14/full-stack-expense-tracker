b0a63f295efdff58db7fc0f3b4a4c2e828fe61ae
vikash rajpoot <vikashraj1490@gmail.com>
2024-02-03 01:44:06 +0530

    signu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import SignUp from './components/SignUp';
 function App() {
 
   return (
-    <>
+    <div className=' h-screen flex justify-center items-center'>
      <SignUp/>
-    </>
+    </div>
   )
 }
 
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 function SignUp() {
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="confirmpass" placeholder="Confirm Password" />
-        <button type="submit">Sign Up</button>
+      <form className='flex border-2 p-4 flex-col justify-center items-center gap-4'>
+        <p className='text-xl mb-2 p-2'>SignUp</p>
+        <input className='px-8 p-2 text-lg border-2' type="text" placeholder="Email" />
+        <input className='px-8 p-2 text-lg border-2' type="password" placeholder="Password" />
+        <input className='px-8 p-2 mb-4 text-lg border-2' type="confirmpass" placeholder="Confirm Password" />
+        <button className='w-full mb-4 bg-sky-500 rounded-full text-white p-1' type="submit">Sign Up</button>
       </form>
+      <div className='border-2 my-2 p-2 flex justify-center cursor-pointer items-center bg-green-100 hover:border-green-200'>Have an account?Login</div>
     </div>
   )
 }
